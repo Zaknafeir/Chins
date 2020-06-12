6851e5dd04501e61c86efc28b66f14c6c1cccc05
Zaknafeir <soul_reaver@bk.ru>
2020-06-12 18:12:09 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,7 @@
+<?
+$sum = hello();
+function hello($a=1, $b=4){
+    $c = $a * $b;
+ return $c*3;
+}
+echo $sum;
---
+++ portfolio/data.php
@@ -0,0 +1,24 @@
+<?php
+return [
+  'about'=>
+      ['name'=>'Ronya',
+       'post'=>'Chinchilla',
+       'email'=>'nyaka_myanyaka@bk.ru',
+       'place'=>'vitrinka2'],
+    'education'=>
+    [
+        ['facultet'=>'cuteies',
+         'university'=> 'Chinchillas schoool',
+         'yearstart'=>2017,
+         'yearend' =>2018
+        ],
+        ['facultet'=>'braves',
+         'university'=> 'Chinchillas highschool',
+         'yearstart'=>2019,
+         'yearend' =>2020
+        ],
+    ],
+    'lang'=> ['English','Russian','NyaNya'],
+    'hobby'=>['prig-skok','chews','Leorick'],
+    'aboutCaree'=>'Neshnaya chinchillka'
+];
